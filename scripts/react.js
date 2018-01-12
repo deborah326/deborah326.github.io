5197a836b126289afdedc4bbc850be151586f7b1
Deborah Leong <deborah326@gmail.com>
2018-01-12 19:50:06 +0800

    init react

---
+++ scripts/react.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+/*
+import React from 'react';
+import { render } from 'react-dom';
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
+
+const photos = [
+  { src: "images/femaleportrait.jpg", width: 4, height: 3 },
+  { src: "images/oldmanportrait.jpg", width: 1, height: 1 },
+  { src: "images/maleportrait.jpg", width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
+  { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
+  { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
+  { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
+];
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { currentImage: 0 };
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+  }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Gallery photos={photos} onClick={this.openLightbox} />
+        <Lightbox images={photos}
+          onClose={this.closeLightbox}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        />
+      </div>
+    )
+  }
+}
+render(<App />, document.getElementById('app'));
+*/
