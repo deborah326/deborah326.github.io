c5dce3859003af12feaeacaa276a4dfa6e277e3c
Deborah Leong <deborah326@gmail.com>
2018-01-12 19:55:39 +0800

    update react index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( <
+  h1 > Hello, world! < /h1>,
+  document.getElementById('root')
+);
+
 $(document).ready(function() {
 
   $("nav").find("li").on("click", "a", function() {
